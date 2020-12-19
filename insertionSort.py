0a2184d9205b4ba8f88113e2f9bb56f33869b76f
Karan Mehra <33761778+ernestomehra@users.noreply.github.com>
2020-12-19 23:21:40 +0530

    Update insertionSort.py

--- insertionSort.py
+++ insertionSort.py
@@ -1,1 +1,22 @@
 
+def insertionsort(arr):
+  for i in range (1, len(arr)):
+    temp = arr[i] 
+    k = i
+    while  k > 0 and temp < arr[k-1]:
+      arr[k] = arr[k-1]
+      k -= 1
+    arr[k] = temp
+  return arr
+
+
+
+def insertionsort1(arr):
+  for i in range (1, len(arr)):
+    temp = arr[i]
+    k = i - 1
+    while k > 0 and temp < arr[k]:
+      arr[k+1] = arr[k]
+      k -= 1
+    arr[k+1] = temp 
+  return arr 
